e1843fc4f29442013f1e0d53b7e6be5b42ab02e4
Foad Lind <foad.lind@citynetwork.eu>
2022-04-01 11:31:46 +0200

    fix: Copy Caddy data if data directory is mounted
    Only backup and restore Caddy data if Caddy data directory is mounted.

--- tutorbackup/templates/backup/build/backup/backup_services.sh
+++ tutorbackup/templates/backup/build/backup/backup_services.sh
@@ -4,5 +4,7 @@ mysqldump --all-databases --single-transaction --quick --quote-names --max-allow
   --password=$MYSQL_ROOT_PASSWORD --user=$MYSQL_ROOT_USERNAME --host=$MYSQL_HOST --port=$MYSQL_PORT > /data/mysql_dump.sql
 mongodump --out=/data/mongodb_dump --host=$MONGODB_HOST \
   --port=$MONGODB_PORT {% if MONGODB_USERNAME and MONGODB_PASSWORD %}--username=$MONGODB_USERNAME --password=$MONGODB_PASSWORD{% endif %}
-cp -r caddy/ data/
+if [ -d "caddy" ]; then
+  cp -r caddy/ data/
+fi
 tar --exclude='/data/caddy/locks' -Jcvf /backup/backup.tar.xz /data/
