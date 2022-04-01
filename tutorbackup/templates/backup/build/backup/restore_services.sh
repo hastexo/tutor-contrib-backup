72744dd3a671a129fd01bc5721583474425081cc
Foad Lind <foad.lind@citynetwork.eu>
2022-04-01 11:14:11 +0200

    feat: Backup and restore an external MongoDB
    Add ability to backup and restore a MongoDB running outside Tutor.

--- tutorbackup/templates/backup/build/backup/restore_services.sh
+++ tutorbackup/templates/backup/build/backup/restore_services.sh
@@ -2,5 +2,6 @@
 set -e
 tar -xvf backup/backup.tar.xz
 mysql --password=$MYSQL_ROOT_PASSWORD --user=$MYSQL_ROOT_USERNAME --host=$MYSQL_HOST --port=$MYSQL_PORT < data/mysql_dump.sql
-mongorestore --host=$MONGODB_HOST --port=$MONGODB_PORT --stopOnError /data/mongodb_dump/
+mongorestore --host=$MONGODB_HOST --port=$MONGODB_PORT \
+  {% if MONGODB_USERNAME and MONGODB_PASSWORD %}--username=$MONGODB_USERNAME --password=$MONGODB_PASSWORD {% endif %}--stopOnError /data/mongodb_dump/
 cp -r /data/caddy/* /caddy/
