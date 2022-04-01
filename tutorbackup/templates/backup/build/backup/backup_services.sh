1404062e9bf86c083c079786c125ab6fc1e3fadf
Foad Lind <foad.lind@citynetwork.eu>
2022-04-01 13:58:58 +0200

    feat: Drop MySQL databases before restoring them
    Make mysqldump drop databases before recreating them. We need to
    add --events and --routines because as of MySQL 8.0, mysqldump
    does not dump mysql.proc and mysql.event tables by default.
    Those tables are used in default Tutor MySQL service (version 5.7).

--- tutorbackup/templates/backup/build/backup/backup_services.sh
+++ tutorbackup/templates/backup/build/backup/backup_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-mysqldump --all-databases --single-transaction --quick --quote-names --max-allowed-packet=16M \
+mysqldump --all-databases --add-drop-database --routines --events --single-transaction --quick --quote-names --max-allowed-packet=16M \
   --password=$MYSQL_ROOT_PASSWORD --user=$MYSQL_ROOT_USERNAME --host=$MYSQL_HOST --port=$MYSQL_PORT > /data/mysql_dump.sql
 mongodump --out=/data/mongodb_dump --host=$MONGODB_HOST \
   --port=$MONGODB_PORT {% if MONGODB_USERNAME and MONGODB_PASSWORD %}--username=$MONGODB_USERNAME --password=$MONGODB_PASSWORD{% endif %}
