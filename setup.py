3dc17fbf836837c442aedcbabb000f5401c5a262
Foad Lind <foad.lind@citynetwork.eu>
2022-06-15 14:46:30 +0200

    fix: Limit the plugin to Open edX Maple release
    From version 14, Tutor uses the Open edX Nutmeg release. Until we add
    support for that release, limit the plugin to Tutor versions <14.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
-    install_requires=["tutor"],
+    install_requires=["tutor<14"],
     setup_requires=["setuptools-scm"],
     entry_points={
         "tutor.plugin.v0": [
