12ee1ea80b635cd8237990dca93de43ad0736ef8
Foad Lind <foad.lind@citynetwork.eu>
2022-09-07 08:37:59 +0200

    refactor: Remove obsolete scripts
    In PR #13, we consolidated downloading from S3, and restoring the
    services, in one Python script (`restore_services.py`). But we forgot to
    remove the obsolete scripts. Remove those.

--- tutorbackup/templates/backup/build/backup/restore_services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-tar -xvf backup/backup.tar.xz
-mysql --password=$MYSQL_ROOT_PASSWORD --user=$MYSQL_ROOT_USERNAME --host=$MYSQL_HOST --port=$MYSQL_PORT < data/mysql_dump.sql
-mongorestore --host=$MONGODB_HOST --port=$MONGODB_PORT \
-  {% if MONGODB_USERNAME and MONGODB_PASSWORD %}--username=$MONGODB_USERNAME --password=$MONGODB_PASSWORD {% endif %}--stopOnError --drop /data/mongodb_dump/
-if [ -d "/data/caddy" ]; then
-  cp -r /data/caddy/* /caddy/
-fi
